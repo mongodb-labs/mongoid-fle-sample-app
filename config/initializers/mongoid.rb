88385544c86d52c3528d2b3ca62c0f5a5f9e3605
Alex Bevilacqua <alex@alexbevi.com>
2023-08-18 10:22:57 -0400

    Add a mongocrypt_path_helper for use in the mongoid.yml

--- config/initializers/mongoid.rb
+++ config/initializers/mongoid.rb
@@ -9,17 +9,35 @@ Mongoid.configure do
   # It is recommended to use config/mongoid.yml for most Mongoid-related
   # configuration, whenever possible, but if you prefer, you can set
   # configuration values here, instead:
-  # 
+  #
   #   config.log_level = :debug
   #
   # Note that the settings in config/mongoid.yml always take precedence,
   # whatever else is set here.
 end
- 
+
 # Enable Mongo driver query cache for Rack
 # Rails.application.config.middleware.use(Mongo::QueryCache::Middleware)
- 
+
 # Enable Mongo driver query cache for ActiveJob
 # ActiveSupport.on_load(:active_job) do
 #   include Mongo::QueryCache::Middleware::ActiveJob
 # end
+
+# Identify the current environment based on the RUBY_PLATFORMS environment
+# variable, and use this to autopopulate the path to the crypt_shared
+# library at ./vendor/crypt_shared/mongo_crypt_v1.[dll|dynlib|so]
+#
+# If detection fails the default file extension will be Linux's .so
+def mongocrypt_path_helper
+  platform = RUBY_PLATFORM.downcase
+  ext = if RUBY_PLATFORM =~ /mswin32/
+    "dll"
+  elsif RUBY_PLATFORM =~ /darwin/
+    "dylib"
+  else
+    "so" # default to Linux
+  end
+
+  File.join(Rails.root, 'vendor', 'crypt_shared', "mongo_crypt_v1.#{ext}")
+end
